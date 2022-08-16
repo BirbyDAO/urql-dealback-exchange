145ecc86a4dbd7fc6a1a7b21f8c093b1f8793ca5
Berklium Master <berklium-birb@protonmail.com>
2022-08-16 22:20:53 +0300

    test: Remove moduleName mapping

--- scripts/jest/preset.js
+++ scripts/jest/preset.js
@@ -1,21 +1,16 @@
 module.exports = {
   setupFiles: [
-    require.resolve('./setup.js')
+    require.resolve('./setup.js'),
   ],
   clearMocks: true,
   transform: {
     '^.+\\.tsx?$': '@sucrase/jest-plugin',
   },
-  moduleNameMapper: {
-    "^urql$": "<rootDir>/../../node_modules/urql/src",
-    "^(.*-urql)$": "<rootDir>/../../node_modules/$1/src",
-    "^@urql/(.*)/(.*)$": "<rootDir>/../../node_modules/@urql/$1/src/$2",
-    "^@urql/(.*)$": "<rootDir>/../../node_modules/@urql/$1/src",
-  },
+  moduleNameMapper: {},
   watchPlugins: ['jest-watch-yarn-workspaces'],
   testRegex: '(src/.*(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
   coveragePathIgnorePatterns: ['<rootDir>/src/test-utils'],
-  testPathIgnorePatterns: ["<rootDir>/e2e-tests/*"],
+  testPathIgnorePatterns: ['<rootDir>/e2e-tests/*'],
 };
